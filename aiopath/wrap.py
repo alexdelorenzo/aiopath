cf333dae693f6b157c5003845836ccf401bf2e52
alexdelorenzo <you@example.com>
2021-03-08 19:32:13 -0500

    Add type annotations

--- aiopath/wrap.py
+++ aiopath/wrap.py
@@ -1,3 +1,4 @@
+from typing import Callable, Any, Awaitable
 from functools import wraps
 
 try:
@@ -6,7 +7,7 @@ try:
 except ImportError:
   from asyncio import get_running_loop
 
-  async def to_thread(func, *args, **kwargs):
+  async def to_thread(func: Callable, *args, **kwargs) -> Any:
     loop = get_running_loop()
     return await loop.run_in_executor(
       None,
@@ -16,17 +17,22 @@ except ImportError:
     )
 
 
-def func_as_method_coro(func):
+CoroutineResult = Awaitable[Any]
+CoroutineFunction = Callable[..., CoroutineResult]
+CoroutineMethod = Callable[..., CoroutineResult]
+
+
+def func_as_method_coro(func: Callable) -> CoroutineMethod:
   @wraps(func)
-  async def method(self, *args, **kwargs):
+  async def method(self, *args, **kwargs) -> Any:
     return await to_thread(func, *args, **kwargs)
 
   return method
 
 
-def coro_as_method_coro(coro):
+def coro_as_method_coro(coro: CoroutineFunction) -> CoroutineMethod:
   @wraps(coro)
-  async def method(self, *args, **kwargs):
+  async def method(self, *args, **kwargs) -> Any:
     return await coro(*args, **kwargs)
 
   return method
