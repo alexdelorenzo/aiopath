fb6a862dcd35f54dc381d76286daff8fd4e44fc4
alexdelorenzo <you@example.com>
2021-04-12 16:59:25 -0400

    Fix indentation

--- aiopath/types.py
+++ aiopath/types.py
@@ -5,7 +5,7 @@ try:
 
 
   TextMode = \
-  Literal['r', 'w', 'a', 'x', 'r+', 'w+', 'a+', 'x+']
+    Literal['r', 'w', 'a', 'x', 'r+', 'w+', 'a+', 'x+']
   BinaryMode = \
     Literal['rb', 'wb', 'ab', 'xb', 'r+b', 'w+b', 'a+b', 'x+b']
   FileMode = Union[TextMode, BinaryMode]
@@ -22,5 +22,6 @@ except ImportError:
 
   class Literal(Generic[T]):
     pass
+
 
   FileMode = Literal[str]
