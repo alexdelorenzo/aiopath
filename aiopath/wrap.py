c42d41805b6eab90a797711c63bcb2b7d86ec52e
alexdelorenzo <you@example.com>
2021-04-12 17:37:05 -0400

    Make to_thread 3.7 compatible

--- aiopath/wrap.py
+++ aiopath/wrap.py
@@ -10,11 +10,19 @@ try:
 except ImportError:
   from asyncio import get_running_loop
 
-  async def to_thread(func: Callable, /, *args, **kwargs) -> Any:
-    loop = get_running_loop()
-    ctx = contextvars.copy_context()
-    func_call = partial(ctx.run, func, *args, **kwargs)
-    return await loop.run_in_executor(None, func_call)
+  try:
+    async def to_thread(func: Callable, /, *args, **kwargs) -> Any:
+      loop = get_running_loop()
+      ctx = contextvars.copy_context()
+      func_call = partial(ctx.run, func, *args, **kwargs)
+      return await loop.run_in_executor(None, func_call)
+
+  except SyntaxError:
+    async def to_thread(func: Callable, *args, **kwargs) -> Any:
+      loop = get_running_loop()
+      ctx = contextvars.copy_context()
+      func_call = partial(ctx.run, func, *args, **kwargs)
+      return await loop.run_in_executor(None, func_call)
 
 
 CoroutineResult = Awaitable[Any]
