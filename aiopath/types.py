ad5b899f7e9b2914eca308d2654f7844fe0c91f8
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2021-04-21 20:45:14 -0400

    Literal and Final ship with Python 3.10, remove their redefinitions

--- aiopath/types.py
+++ aiopath/types.py
@@ -1,27 +1,8 @@
-from typing import Optional, List, Union, AsyncIterable
-
-try:
-  from typing import Literal, Final
+from typing import Literal, Final, Union
 
 
-  TextMode = \
-    Literal['r', 'w', 'a', 'x', 'r+', 'w+', 'a+', 'x+']
-  BinaryMode = \
-    Literal['rb', 'wb', 'ab', 'xb', 'r+b', 'w+b', 'a+b', 'x+b']
-  FileMode = Union[TextMode, BinaryMode]
-
-except ImportError:
-  from typing import TypeVar, Generic
-
-
-  T = TypeVar('T')
-
-
-  class Final(Generic[T]):
-    pass
-
-  class Literal(Generic[T]):
-    pass
-
-
-  FileMode = Literal[str]
+TextMode = \
+  Literal['r', 'w', 'a', 'x', 'r+', 'w+', 'a+', 'x+']
+BinaryMode = \
+  Literal['rb', 'wb', 'ab', 'xb', 'r+b', 'w+b', 'a+b', 'x+b']
+FileMode = Union[TextMode, BinaryMode]
