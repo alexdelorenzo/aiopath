5c40e7ba8c5e464efe35b2198fb8f859eb10bfaa
alexdelorenzo <you@example.com>
2021-05-03 21:08:46 -0400

    Fix indentation

--- aiopath/types.py
+++ aiopath/types.py
@@ -7,8 +7,8 @@ except ImportError:
   from typing_extensions import Literal, Final
 
 
-  TextMode = \
-    Literal['r', 'w', 'a', 'x', 'r+', 'w+', 'a+', 'x+']
-  BinaryMode = \
-    Literal['rb', 'wb', 'ab', 'xb', 'r+b', 'w+b', 'a+b', 'x+b']
-  FileMode = Union[TextMode, BinaryMode]
+TextMode = \
+  Literal['r', 'w', 'a', 'x', 'r+', 'w+', 'a+', 'x+']
+BinaryMode = \
+  Literal['rb', 'wb', 'ab', 'xb', 'r+b', 'w+b', 'a+b', 'x+b']
+FileMode = Union[TextMode, BinaryMode]
