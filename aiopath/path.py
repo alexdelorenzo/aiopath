810530e73f41ca38e96f7833f59d7d09144f443d
alexdelorenzo <you@example.com>
2021-05-03 20:59:53 -0400

    Update annotations

--- aiopath/path.py
+++ aiopath/path.py
@@ -438,8 +438,8 @@ class AsyncPath(Path, AsyncPurePath):
     if drv or root:
       raise NotImplementedError("Non-relative patterns are unsupported")
 
-    #selector = _make_selector(("**",) + tuple(pattern_parts), self._flavour)
-    parts = ("**", *pattern_parts)
+    # selector = _make_selector(("**",) + tuple(pattern_parts), self._flavour)
+    parts: tuple[str, ...] = ("**", *pattern_parts)
     selector = _make_selector(parts, self._flavour)
 
     async for p in selector.select_from(self):
