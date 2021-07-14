dc42e61889916d81afffcadf8780c4feed9b7285
alexdelorenzo <alex@alexdelorenzo.dev>
2021-07-14 18:55:31 -0400

    Refactor test

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -18,26 +18,30 @@ from . import _test_is, _test_is_io, _test_is_pure, \
 @pytest.mark.asyncio
 async def test_issue_13(file_paths: Paths):
   _, apath = file_paths
-  chunk_size: int = 10
-  text = printable
 
+  chunk_size: int = 10
+  text: str = printable
+  data: bytes = text.encode()
+
+  first_slice = slice(0, chunk_size)
+  next_slice = slice(chunk_size, chunk_size * 2)
+
+  # test read/write text
   await apath.write_text(text)
 
   async with apath.open('r') as file:
     chunk = await file.read(chunk_size)
-    assert chunk == text[:chunk_size]
+    assert chunk == text[first_slice]
 
     chunk = await file.read(chunk_size)
-    next_slice = slice(chunk_size, chunk_size * 2)
     assert chunk == text[next_slice]
 
-  data = text.encode()
+  # test read/write bytes
   await apath.write_bytes(data)
 
   async with apath.open('rb') as file:
     chunk = await file.read(chunk_size)
-    assert chunk == data[:chunk_size]
+    assert chunk == data[first_slice]
 
     chunk = await file.read(chunk_size)
-    next_slice = slice(chunk_size, chunk_size * 2)
     assert chunk == data[next_slice]
