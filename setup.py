26f31452abfd6a609ea64ef07a959f9ff6a311b0
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-10-12 17:16:26 -0400

    Pull expressions into constants

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ from pathlib import Path
 
 
 PKG_NAME: str = "aiopath"
-NAME: str = 'aiopath'
+NAME: str = "aiopath"
 VERSION: str = "0.7.4"
 LICENSE: str = "LGPL-3.0"
 
 DESC: str = "📁 Async pathlib for Python"
 
-REQUIREMENTS: list[str] = \
-  Path('requirements.txt') \
-    .read_text() \
-    .splitlines()
-
-README: str = Path('README.md').read_text()
+REQUIREMENTS: list[str] = (
+  Path("requirements.txt")
+  .read_text()
+  .splitlines()
+)
+README: str = Path("README.md").read_text()
+PYTHON_VERSIONS: str = ">=3.12"
+GITHUB_URL: str = "https://github.com/AlexDeLorenzo"
 
 
 setup(
@@ -24,12 +26,12 @@ setup(
   description=DESC,
   long_description=README,
   long_description_content_type="text/markdown",
-  url='https://github.com/alexdelorenzo/aiopath',
+  url=GITHUB_URL,
   author=__author__,
   license=LICENSE,
   packages=[NAME],
   zip_safe=True,
   install_requires=REQUIREMENTS,
-  python_requires='>=3.12',
+  python_requires=PYTHON_VERSIONS,
   include_package_data=True
 )
