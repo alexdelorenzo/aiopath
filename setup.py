366fe9996dd71f2c8f76026152d55ba674b6b2f8
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-10-12 17:10:58 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 PKG_NAME: str = "aiopath"
 NAME: str = 'aiopath'
-VERSION: str = "0.7.3"
+VERSION: str = "0.7.4"
 LICENSE: str = "LGPL-3.0"
 
 DESC: str = "📁 Async pathlib for Python"
